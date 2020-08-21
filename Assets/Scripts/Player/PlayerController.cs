8a2b6bc192a926501bde79a195c3d0c62e067b6e
sebprest <38880204+sebprest@users.noreply.github.com>
2020-08-21 23:23:39 +0100

    Add basic mouse look to player controller script

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -5,16 +5,23 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 12f;
-    CharacterController controller;
+    public float mouseSensitivity;
+
+    private float cameraAngleY = 0f;
+
+    private Camera playerCamera;
+    private CharacterController controller;
 
     private void Awake()
     {
         controller = GetComponent<CharacterController>();
+        playerCamera = transform.Find("Main Camera").GetComponent<Camera>();
     }
 
     private void Update()
     {
         HandleMovement();
+        HandleMouseLook();
     }
 
     private void HandleMovement()
@@ -25,5 +32,19 @@ public class PlayerController : MonoBehaviour
         Vector3 move = transform.right * x + transform.forward * z;
 
         controller.Move(move * speed * Time.deltaTime);
+    }
+
+    private void HandleMouseLook()
+    {
+        float mouseX = Input.GetAxisRaw("Mouse X");
+        float mouseY = Input.GetAxisRaw("Mouse Y");
+
+        transform.Rotate(Vector3.up * mouseX * mouseSensitivity);
+
+        cameraAngleY -= mouseY * mouseSensitivity;
+        // Clamp xRotation to prevent the camera pointing behind the player's head
+        cameraAngleY = Mathf.Clamp(cameraAngleY, -90f, 90f);
+
+        playerCamera.transform.rotation = Quaternion.Euler(cameraAngleY, 0f, 0f);
     }
 }
