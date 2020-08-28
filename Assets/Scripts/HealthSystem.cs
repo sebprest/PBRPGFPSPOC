f3f6f671e26f2d8ad31b61ee735dc7a615672021
Sebastian Preston <38880204+sebprest@users.noreply.github.com>
2020-08-28 20:47:35 +0100

    Create HealthSystem class to handle health of living entities

---
+++ Assets/Scripts/HealthSystem.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthSystem
+{
+    private int currentHealth;
+
+    public HealthSystem(int maxHealth)
+    {
+        currentHealth = maxHealth;
+    }
+
+    public int GetHealth()
+    {
+        return currentHealth;
+    }
+
+    public void Damage(int amount)
+    {
+        currentHealth -= amount;
+    }
+}
